6b4bae0424c2455c57a4012b94931570dac228fe
hemanthkumar882002 <hemanthkumar882002@gmail.com>
2026-06-02 13:08:46 +0530

    modified python

--- pythoncode.py
+++ pythoncode.py
@@ -1,3 +1,10 @@
-print "hello i am learing git"
+print ('hello hemanth');
+print('hello world')
+import pandas as pd;
+df=pd.read_csv(r"C:\Users\AISHU\Music\orders.csv")
+df.head()
+print(df.head())
+print(df)
+
 
 
